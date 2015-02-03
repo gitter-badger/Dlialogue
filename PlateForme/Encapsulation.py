470eb257054b0bece4ea14b391d12d7edbd6d495
baptiste <baptiste.galton@gmail.com>
2015-02-03 18:38:47 +0100

    pour la neige

--- PlateForme/Encapsulation.py
+++ PlateForme/Encapsulation.py
@@ -5,14 +5,11 @@ import time
 from ServicesWeb import ServiceWebInput, ServiceWebOutput
 from threading import Thread
 
-#ports are meant to finally be in the directory
-hubPort = "2000"
-outputPort = "2001"
-
 class Kanban :
     def __init__(self,orchestrationName):
         '''should initiate state thanks to the orchestrationName'''
         self.orchestrationName = orchestrationName
+        self.idDialogue = ""
         self.state = ""
         self.idMessage = ""
 
@@ -22,43 +19,45 @@ class Encapsulateur:
 
 class EncapsulateurLocal(Encapsulateur):
     """Encapsule une fonction locale"""
-    def __init__(self,context, port, worker):
-      self.context = context
-      socket = context.socket(zmq.PAIR)
+    def __init__(self,contexte, port, worker):
+      self.contexte = contexte
+      socket = contexte.socket(zmq.PAIR)
       socket.bind("tcp://*:%s" % port)
     pass
 
 class EncapsulateurExempleInput(Thread):
     """Encapsule une fonction input qui pourrait être un service web"""
 
-    def __init__(self,context, port):
+    def __init__(self,contexte, port):
         Thread.__init__(self)
-        self.context = context
-        self.socket = context.socket(zmq.PUSH)
-        self.socket.connect("tcp://127.0.0.1:%s" % hubPort)
+        self.contexte = contexte
+        self.socket = contexte.socket(zmq.PUSH)
+        self.socket.connect("tcp://127.0.0.1:%s" % port)
+        # self.socket.bind("tcp://localhost:%s" % port)
 
     def run(self):
         print("coucou input")
-        for lettre in "chaine":
+        for i in "chaine":
             message = ServiceWebInput.main()
-            print(message)
+            print("Input sending : ", message)
             self.socket.send(message.encode('utf8'))
-            time.sleep(5)
+            time.sleep(2)
 
 class EncapsulateurExempleOutput(Thread):
     """Encapsule une fonction output qui pourrait être un service web"""
-
-    def __init__(self,context, port):
+    def __init__(self,contexte, port):
         Thread.__init__(self)
-        self.context = context
-        self.socket = context.socket(zmq.PULL)
-        self.socket.bind("tcp://127.0.0.1:%s" % outputPort)
+        self.contexte = contexte
+        self.socket = contexte.socket(zmq.PULL)
+        self.socket.bind("tcp://127.0.0.1:%s" % port)
+        # self.socket.bind("tcp://localhost:%s" % 1234)
 
     def run(self):
+        print ("Output : Ready to receive")
         # self.socket.bind("tcp://localhost:5557")
         while True:
-            print ("Pret à recevoir")
             message = self.socket.recv()
+            print ("Output receiving : ", message)
             ServiceWebOutput.main(message)
         pass
 
@@ -75,23 +74,18 @@ class Hub(Thread):
         self.context = context
 
         self.universalInputSocket = context.socket(zmq.PULL)
-        self.universalInputSocket.bind("tcp://127.0.0.1:%s" % hubPort)
+        self.universalInputSocket.bind("tcp://127.0.0.1:%s" % directory["Exemple-Hub"])
         #self.universalInputSocket.bind("127.0.0.1:%s" % self.port)
 
         #temp : just a static unique port accessing an encapsulator
         self.outputSocket = context.socket(zmq.PUSH)
-        self.outputSocket.connect("tcp://127.0.0.1:%s" % outputPort)
+        self.outputSocket.connect("tcp://127.0.0.1:%s" % directory["Exemple-Output"])
 
 
     def run(self):
+        #print (self.directory())
         while True:
             message = self.universalInputSocket.recv()
             #shunt of the message : for now it's static
             self.outputSocket.send(message)
-
-
-
-
-
-
 
--- PlateForme/Main.py
+++ PlateForme/Main.py
@@ -1,73 +1,106 @@
 __author__ = 'baptiste'
 #-*- coding:utf-8 -*-
 import zmq
+import json
+from collections import defaultdict
 from threading import Thread
 from PlateForme.Encapsulation import EncapsulateurExempleOutput, EncapsulateurExempleInput, Hub
 
-orchestrations = {}
-
-class orchestration :
-    '''orchestration defined with its state chart and its unique name as id'''
-    def __init__(self,orchestrationStateChart,orchestrationName):
-        self.orchestrationStateChart = orchestrationStateChart
-        self.orchestrationName = orchestrationName #unique
-        self.directory = {} #directory : worker/port
-        #self.context ??
-
 #Orchestrations Management : add/check/modify/delete
-
 #Local Services Management : add/check/modify/delete
-
 #Local Enconders Management : add/check/modify/delete
-
 #Orchestrations Launcher : start/stop, threads management
-def StartOrchestration(orchestration):
-    pass
 
+global idDialogueIncrement
+idDialogueIncrement = 0
+orchestrations = {"orchestrationName":"json"}
+
+directory = {"encapsulationName":"address"}
+forbiddenPorts = ["8080"]
+
+idDialogueOrchestration = {"idDialogue":"orchestrationName"}
+dialogueStates = {"idDialogue":"state"}
+
+
+threads = {"orchestrationName":["thread1","thread2"]}
+
+blackBoard = {"idDialogue":{"idTurn":{"attribut":"valeur"}}}
+
+def initialization():
+    '''reads folders, finds id max for idDialogueIncrement, checks that '''
+
+    orchestrationName = "firstExample"
+    path = "../Orchestrations/"+orchestrationName+".json"
+    json_data=open(path).read()
+    orchestration = json.loads(json_data)
+    orchestrations.update ({orchestrationName : orchestration})
+    # print(orchestrations[orchestrationName])
+
+
+def getOrchestration(orchestrationName):
+    path = "../Orchestrations/"+orchestrationName+".json"
+    json_data=open(path).read()
+    orchestration = json.loads(json_data)
+    return orchestration
+
+def addressPorts(orchestration):
+    if "Hub" not in orchestrations :
+        directory["Hub"] = "273"
+    print (directory["Hub"])
+    for state in orchestration["states"]:
+            print (state)
+            for substate in orchestration["states"][state]["parallelSubstates"]:
+                print (substate)
+
+def StartOrchestration(orchestration):
+    print ("start")
+    listeWorkers
+    pass
 def StopOrchestration(orchestration):
     pass
 
-def DemarrageProcessusExemple():
-    '''as long as the orchestration management is not dynamic and the orchestration unique, this function will exist'''
+def LaunchingExample(orchestrationName):
     try:
+        print("Launching static example")
+
         context = zmq.Context()
 
-        print("salut les copains")
+        path = "../Orchestrations/"+orchestrationName+".json"
+        json_data=open(path).read()
+        orchestration = json.loads(json_data)
 
-        #Instanciation du (d'un) point de sortie
-        portOutput = "5557"
+        for state in orchestration["states"]:
+            print (state)
+            for substate in orchestration["states"][state]["parallelSubstates"]:
+                print (substate)
+
+        directory = {"Exemple-Hub":"4500", "Exemple-Output":"5556"}
         # output = EncapsulateurExempleOutput(context, portOutput)
-        threadOutput = EncapsulateurExempleOutput(context, portOutput)
+        threadOutput = EncapsulateurExempleOutput(context, directory["Exemple-Output"])
         # Thread(target=output.run(), args=())
-
         #Instanciation de la suite du processus : PUSH PULL
         #Fusion/Fission -> zmq.PUB/SUB
-
-        # #Instanciation du (d'un) point d'entrée
-        portInput = "5556"
         # input = EncapsulateurExempleInput(context, portInput)
-        threadInput = EncapsulateurExempleInput(context, portInput)
+        threadInput = EncapsulateurExempleInput(context, directory["Exemple-Hub"])
             # Thread(target=input.run(), args=())
-
         #Instanciation d'un hub
-        threadHub = Hub(context,"","")
-
+        threadHub = Hub(context,"",directory)
         #Démarrage des threads
         threadOutput.start()
         threadHub.start()
         threadInput.start()
-
-
         # Attend que les threads se terminent
         threadInput.join()
         threadOutput.join()
-
     except (RuntimeError, TypeError, NameError):
         print ("bringing down zmq device")
         pass
 
-DemarrageProcessusExemple()
+#     DemarrageProcessusExemple()
+# orchestration = getOrchestration("firstExample")
+# addressPorts(orchestration)
+# LaunchingExample(orchestration)
 
 
-
-
+initialization()
+StartOrchestration(orchestrations["firstExample"])
